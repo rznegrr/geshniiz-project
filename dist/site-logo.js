bb7297d2a5a2b22d7101669234b7b74b676d70cc
Negar <negar@Negars-MacBook-Air.local>
2024-08-27 14:28:58 -0700

    fixed some bugs

---
+++ dist/site-logo.js
@@ -0,0 +1,9 @@
+const SiteLogo = () => {
+    return (
+        <div className="md:col-span-4 col-span-12 mx-auto">
+            <img src="images/logo-site-header.svg" alt="logo-site" />
+        </div>
+    );
+}
+
+export default SiteLogo;
--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  output: 'export',
-  reactStrictMode: true,
-  distDir: 'dist',
-  images: {
-    unoptimized: true,
-  }
-}
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-module.exports = nextConfig
+export default nextConfig;
